31025530346a2e75363299a8b842d3cb5265c12d
obada halabi <obadaalabi95@gmail.com>
2023-04-02 23:28:40 +0200

    Add project files.

---
+++ ApiData/NewsApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewsApiData
+{
+    public class NewsApiDbContext : DbContext
+    {
+        public NewsApiDbContext(DbContextOptions<NewsApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
