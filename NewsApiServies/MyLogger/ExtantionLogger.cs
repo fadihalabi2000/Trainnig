fb1add6ff71372a4edc345272bc99b48b59a0bdf
obada halabi <obadaalabi95@gmail.com>
2023-04-13 23:44:53 +0200

    AddLogger

---
+++ NewsApiServies/MyLogger/ExtantionLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Services.MyLogger
+{
+    public static class ExtantionLogger
+    {
+        public static void LogErorr(this Ilogger logger, string message) 
+        {
+            logger.log(message, "Erorr");
+        }
+        public static void LogWarning(this Ilogger logger, string message)
+        {
+            logger.log(message, "Warning");
+        }
+        public static void LogInformation(this Ilogger logger, string message)
+        {
+            logger.log(message, "Information");
+        }
+    }
+}
